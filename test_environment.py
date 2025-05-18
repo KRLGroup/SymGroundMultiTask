3eef3956d61f71e75c674d262511bc63839279d0
Matteo Pannacci <matteo.pannacci@gmail.com>
2025-05-19 00:16:11 +0200

    implemented commands with keyboard

--- test_environment.py
+++ test_environment.py
@@ -1,6 +1,7 @@
 import torch
 import argparse
 import utils
+import cv2
 
 if torch.cuda.is_available():
     device = 'cuda'
@@ -9,18 +10,18 @@ else:
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--env", default="GridWorld-v0",
-                help="name of the environment to train on (default: GridWorld-v0)")
+parser.add_argument("--env", default="GridWorld-v0")
+parser.add_argument("--input_type", default="keyboard", choices=["keyboard", "terminal"])
 args = parser.parse_args()
 
 
 env = utils.make_env(args.env, progression_mode="full", ltl_sampler="None", seed=1, intrinsic=0, noLTL=False, device=device)
+input_type = args.input_type
 str_to_action = {"s":0,"d":1,"w":2,"a":3}
 
 obs = env.reset()
 done = False
 step = 0
-
 
 while not done:
 
@@ -32,22 +33,43 @@ while not done:
     print(f"Task: {obs['text']}")
 
     print("\nAction: ", end="")
-    a = input()
-    while a not in str_to_action:
-        print("invalid action...")
-        print("Action: ", end="")
-        a = input()
-    a = str_to_action[a]
 
-    obs, reward, done, info = env.step(a%env.action_space.n)
+    if input_type == "terminal":
+        a = input()
+        while a not in str_to_action:
+            print("invalid action...")
+            print("Action: ", end="")
+            a = input()
+
+    elif input_type == "keyboard":
+        a = None
+        while a is None:
+            key = cv2.waitKey(100)
+            if key == 81 or key == ord('a'):
+                a = "a"
+            elif key == 82 or key == ord('w'):
+                a = "w"
+            elif key == 83 or key == ord('d'):
+                a = "d"
+            elif key == 84 or key == ord('s'):
+                a = "s"
+        print(a)
+
+    a = str_to_action[a]
+    obs, reward, done, info = env.step(a)
 
     if done:
         env.show()
         print(f"Reward: {reward}")
         print("Done!")
-        input("Closing...")
+        print("Closing...")
         break
 
     print(f"Reward: {reward}")
+
+if input_type == "terminal":
+    input()
+elif input_type == "keyboard":
+    cv2.waitKey(0)
 
 env.close()
