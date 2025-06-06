505c73fb5fa324be3e490950229bf67f8d4ea6aa
Matteo Pannacci <matteo.pannacci@gmail.com>
2025-06-06 17:20:20 +0200

    added formula visualization from sampler

--- visualize_task.py
+++ visualize_task.py
@@ -2,14 +2,16 @@ import pickle
 import os
 import argparse
 import ast
-from utils import ltl2dfa
+from utils import ltl2dfa, pprint_ltl_formula
+from ltl_samplers import getLTLSampler
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--mode", type=str, default="file", choices=["file", "manual", "sampler"])
 parser.add_argument("--folder", type=str, default="envs/gridworld_multitask/tasks")
 parser.add_argument("--id", type=int, default=0)
-parser.add_argument("--sampler", type=str, default="keyboard", choices=["keyboard", "terminal"])
+parser.add_argument("--symbols", nargs="+", type=str, default=["a", "b", "c", "d", "e"])
 parser.add_argument("--formula", type=ast.literal_eval, default=('eventually', 'b'))
+parser.add_argument("--sampler", type=str, default="Eventually_1_5_1_4")
 args = parser.parse_args()
 
 MAIN_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -25,16 +27,19 @@ if args.mode == "file":
     with open(os.path.join(TASKS_DIR, "automata.pkl"), "rb") as f:
         automaton = pickle.load(f)[args.id]
 
-
 elif args.mode == "manual":
 
     formula = tuple(args.formula)
-    automaton = ltl2dfa(formula, ["a", "b", "c", "d", "e"])
-
+    automaton = ltl2dfa(formula, args.symbols)
 
 elif args.mode == "sampler":
-    raise NotImplementedError
+
+    sampler = getLTLSampler(args.sampler, args.symbols)
+    formula = sampler.sample()
+    print(formula)
+    automaton = ltl2dfa(formula, args.symbols)
 
 
-print(f"Formula: {formula}")
+print("Formula:")
+pprint_ltl_formula(formula)
 automaton.write_dot_file(os.path.join(OUTPUT_DIR, "automaton"), show=True)
