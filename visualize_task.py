782bab7f3cddfa5b02c6cea2f84d7daaef4daa93
Matteo Pannacci <matteo.pannacci@gmail.com>
2025-06-06 20:09:34 +0200

    added parameter to not use automaton

--- visualize_task.py
+++ visualize_task.py
@@ -12,6 +12,8 @@ parser.add_argument("--id", type=int, default=0)
 parser.add_argument("--symbols", nargs="+", type=str, default=["a", "b", "c", "d", "e"])
 parser.add_argument("--formula", type=ast.literal_eval, default=('eventually', 'b'))
 parser.add_argument("--sampler", type=str, default="Eventually_1_5_1_4")
+parser.add_argument('--automaton', dest='automaton', action='store_true')
+parser.add_argument('--no-automaton', dest='automaton', action='store_false')
 args = parser.parse_args()
 
 MAIN_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -19,27 +21,31 @@ OUTPUT_DIR = os.path.join(MAIN_DIR, "saves")
 
 
 if args.mode == "file":
-
     TASKS_DIR = os.path.join(MAIN_DIR, args.folder)
-
     with open(os.path.join(TASKS_DIR, "formulas.pkl"), "rb") as f:
         formula = pickle.load(f)[args.id]
-    with open(os.path.join(TASKS_DIR, "automata.pkl"), "rb") as f:
-        automaton = pickle.load(f)[args.id]
 
 elif args.mode == "manual":
-
     formula = tuple(args.formula)
-    automaton = ltl2dfa(formula, args.symbols)
 
 elif args.mode == "sampler":
-
     sampler = getLTLSampler(args.sampler, args.symbols)
     formula = sampler.sample()
-    print(formula)
-    automaton = ltl2dfa(formula, args.symbols)
-
 
 print("Formula:")
 pprint_ltl_formula(formula)
-automaton.write_dot_file(os.path.join(OUTPUT_DIR, "automaton"), show=True)
+
+
+if args.automaton:
+
+    if args.mode == "file":
+        with open(os.path.join(TASKS_DIR, "automata.pkl"), "rb") as f:
+            automaton = pickle.load(f)[args.id]
+
+    if args.mode == "manual":
+        automaton = ltl2dfa(formula, args.symbols)
+
+    if args.mode == "sampler":
+        automaton = ltl2dfa(formula, args.symbols)
+
+    automaton.write_dot_file(os.path.join(OUTPUT_DIR, "automaton"), show=True)
