cbcb33572089334332cbd8f0eb2133a76bf6e2d5
Matteo Pannacci <matteo.pannacci@gmail.com>
2025-07-23 12:25:56 +0200

    updated test_grounder according to new train_grounder

--- test_grounder.py
+++ test_grounder.py
@@ -2,25 +2,23 @@ import argparse
 import torch
 import numpy as np
 import os
+import pickle
 
 import utils
-from envs.gridworld_multitask.Environment import GridWorldEnv_multitask
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--agent_centric', default=True, dest='agent_centric', action='store_true')
-parser.add_argument('--no-agent_centric', dest='agent_centric', action='store_false')
-parser.add_argument("--iters", default=2000, type=int)
+parser.add_argument("--device", default=None, type=str)
+parser.add_argument('--env', default="GridWorld-fixed-v1", type=str)
 parser.add_argument("--grounder", default=None, type=str)
+parser.add_argument("--iters", default=2000, type=int)
 args = parser.parse_args()
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = args.device or ("cuda" if torch.cuda.is_available() else "cpu")
+device = torch.device(device)
 
 REPO_DIR = os.path.dirname(os.path.abspath(__file__))
 
-
-# build environment
-env = GridWorldEnv_multitask(state_type="image", max_num_steps=50, randomize_loc=True, agent_centric_view=args.agent_centric)
 
 # compute grounder dir
 storage_dir = os.path.join(REPO_DIR, "storage")
@@ -31,45 +29,54 @@ with open(os.path.join(grounder_dir, "config.pickle"), "rb") as f:
     config = pickle.load(f)
 print(f"\nConfig:\n{config}")
 
+# build environment
+env = utils.make_env(
+    args.env,
+    progression_mode = "full",
+    ltl_sampler = "Dataset_e54",
+    grounder = None,
+    obs_size = config.obs_size
+)
+
+num_symbols = len(env.env.dictionary_symbols)
+
 # load training status
 status = utils.get_status(grounder_dir, device)
 
 # load grounder
-sym_grounder = utils.make_grounder("ObjectCNN", len(env.dictionary_symbols), config.obs_size)
+sym_grounder = utils.make_grounder("ObjectCNN", num_symbols, config.obs_size)
 sym_grounder.load_state_dict(status["grounder_state"])
 sym_grounder.to(device)
 
 
 # TEST
 
-class_accs = []
+accs = []
 
 for i in range(args.iters):
 
     print(f"iteration {i}")
 
-    _, _, env_images, env_labels = env.reset()
+    env.reset()
     env.sampler.sampled_tasks = -1
 
-    # collect data to compute accuracy on the test enviornment (only for logging)
-    images = []
-    labels = []
-    for c in range(7):
-        for r in range(7):
-            images.append(env_images[r, c])
-            labels.append(env_labels[r, c])
-    images = np.stack(images)
+    coords = env.env.loc_to_label.keys()
+
+    # obtain and preprocess data
+    images = np.stack([env.env.loc_to_obs[(r, c)] for (r, c) in coords])
     images = torch.tensor(images, device=device, dtype=torch.float32)
-    labels = torch.tensor(labels, device=device, dtype=torch.int32)
+    real_syms = [env.env.loc_to_label[(r, c)] for (r, c) in coords]
+    real_syms = torch.tensor(real_syms, device=device, dtype=torch.int32)
 
-    pred_sym = torch.argmax(sym_grounder(images), dim=-1)
-    correct_preds = torch.sum((pred_sym == labels).int())
-    class_acc = torch.mean((pred_sym == labels).float())
+    # predict symbols
+    pred_syms = torch.argmax(sym_grounder(images), dim=-1)
+    correct_preds = torch.sum((pred_syms == real_syms).int())
+    acc = torch.mean((pred_syms == real_syms).float())
 
-    print(f"grounder accuracy = {correct_preds.item()} / {pred_sym.shape[0]} ({class_acc.item():.4f})")
+    print(f"grounder accuracy = {correct_preds.item()} / {pred_syms.shape[0]} ({acc.item():.4f})")
 
-    class_accs.append(class_acc.item())
-    mean_class_acc = torch.mean(torch.tensor(class_accs, device=device, dtype=torch.float32))
-    print(f"cumulative accuracy = {mean_class_acc:.10f}")
+    accs.append(acc.item())
+    mean_acc = torch.mean(torch.tensor(accs, device=device, dtype=torch.float32))
+    print(f"cumulative accuracy = {mean_acc:.10f}")
 
     print("---")
