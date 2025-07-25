65c06c7daf439047c1ab433912eade73919bc571
Matteo Pannacci <matteo.pannacci@gmail.com>
2025-07-25 12:55:04 +0200

    moved GridWorld's LTLWrapper to ltl_wrappers file and renamed it "LTLGrounderEnv"

--- ltl_wrappers.py
+++ ltl_wrappers.py
@@ -52,18 +52,6 @@ class LTLEnv(gym.Wrapper):
         self.intrinsic = intrinsic
 
 
-    def sample_ltl_goal(self):
-        # This function must return an LTL formula for the task
-        # Format:
-        #(
-        #    'and',
-        #    ('until','True', ('and', 'd', ('until','True',('not','c')))),
-        #    ('until','True', ('and', 'a', ('until','True', ('and', 'b', ('until','True','c')))))
-        #)
-        # NOTE: The propositions must be represented by a char
-        raise NotImplementedError
-
-
     def reset(self):
         self.known_progressions = {}
         self.obs = self.env.reset()
@@ -141,8 +129,16 @@ class LTLEnv(gym.Wrapper):
 
 
     def sample_ltl_goal(self):
-        # NOTE: The propositions must be represented by a char
+
         # This function must return an LTL formula for the task
+        # Format:
+        #(
+        #    'and',
+        #    ('until','True', ('and', 'd', ('until','True',('not','c')))),
+        #    ('until','True', ('and', 'a', ('until','True', ('and', 'b', ('until','True','c')))))
+        #)
+        # NOTE: The propositions must be represented by a char
+
         formula = self.sampler.sample()
 
         if isinstance(self.sampler, SequenceSampler):
@@ -174,23 +170,152 @@ class NoLTLWrapper(gym.Wrapper):
         """
         super().__init__(env)
         self.observation_space = env.observation_space
-        # self.observation_space =  env.observation_space['features']
 
 
     def reset(self):
         obs = self.env.reset()
-        # obs = obs['features']
-        # obs = {'features': obs}
         return obs
 
 
     def step(self, action):
-        # executing the action in the environment
         obs, reward, done, info = self.env.step(action)
-        # obs = obs['features']
-        # obs = {'features': obs}
         return obs, reward, done, info
 
 
     def get_propositions(self):
         return list([])
+
+
+
+# A subclass of LTLEnv to distinguish between "real" progrssion and "predicted" progression
+class LTLGrounderEnv(LTLEnv):
+
+    num_envs = 0
+
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.id = LTLGrounderEnv.num_envs
+        LTLGrounderEnv.num_envs += 1
+
+
+    def reset(self):
+    
+        self.real_known_progressions = {}
+        self.pred_known_progressions = {}
+        self.obs = self.env.reset()
+
+        # defining an LTL goal
+        self.ltl_original, self.task_id = self.sample_ltl_goal()
+        self.real_ltl_goal = self.ltl_original
+        self.pred_ltl_goal = self.ltl_original
+
+        # adding the ltl goal to the observation
+        if self.progression_mode == "partial":
+            ltl_obs = {
+                'features': self.obs,
+                'progress_info': self.progress_info(self.pred_ltl_goal),
+                'task_id': self.task_id,
+                'episode_id': self.env.num_episodes,
+                'env_id': self.id
+            }
+        else:
+            ltl_obs = {
+                'features': self.obs,
+                'text': self.pred_ltl_goal,
+                'task_id': self.task_id,
+                'episode_id': self.env.num_episodes,
+                'env_id': self.id
+            }
+
+        return ltl_obs
+
+
+    def step(self, action):
+
+        int_reward = 0
+
+        # executing the action in the environment
+        next_obs, env_reward, env_done, info = self.env.step(action)
+
+        # progressing real ltl formula
+        real_label = self.env.get_real_events()
+        self.real_ltl_goal = self.progression(self.real_ltl_goal, real_label)
+
+        # progressing pred ltl formula
+        pred_label = self.env.get_events()
+        self.pred_ltl_goal = self.progression(self.pred_ltl_goal, pred_label)
+
+        self.obs = next_obs
+
+        # computing real reward and done
+        if self.real_ltl_goal == 'True':
+            real_ltl_reward = 1.0
+            real_ltl_done = True
+        elif self.real_ltl_goal == 'False':
+            real_ltl_reward = -1.0
+            real_ltl_done = True
+        else:
+            real_ltl_reward = 0.0
+            real_ltl_done = False
+
+        # computing pred reward and done
+        if self.pred_ltl_goal == 'True':
+            pred_ltl_reward = 1.0
+            pred_ltl_done = True
+        elif self.pred_ltl_goal == 'False':
+            pred_ltl_reward = -1.0
+            pred_ltl_done = True
+        else:
+            pred_ltl_reward = int_reward
+            pred_ltl_done = False
+
+        # computing the new observation and returning the outcome of this action
+        # the observation considers the expected formula (unless using 'real')
+        if self.progression_mode == "full":
+            ltl_obs = {
+                'features': self.obs,
+                'text': self.pred_ltl_goal,
+                'task_id': self.task_id,
+                'episode_id': self.env.num_episodes,
+                'env_id': self.id
+            }
+        elif self.progression_mode == "none":
+            ltl_obs = {
+                'features': self.obs,
+                'text': self.ltl_original,
+                'task_id': self.task_id,
+                'episode_id': self.env.num_episodes,
+                'env_id': self.id
+            }
+        elif self.progression_mode == "partial":
+            ltl_obs = {
+                'features': self.obs,
+                'progress_info': self.progress_info(self.pred_ltl_goal),
+                'task_id': self.task_id,
+                'episode_id': self.env.num_episodes,
+                'env_id': self.id
+            }
+        elif self.progression_mode == "real":
+            ltl_obs = {
+                'features': self.obs,
+                'progress_info': self.real_ltl_goal,
+                'task_id': self.task_id,
+                'episode_id': self.env.num_episodes,
+                'env_id': self.id
+            }
+        else:
+            raise NotImplementedError
+
+        # the reward considers the real evolution of the formula
+        reward = env_reward + real_ltl_reward
+
+        # the termination checks both real termination or expected one
+        done = env_done or real_ltl_done or pred_ltl_done
+
+        return ltl_obs, reward, done, info
+
+
+    # returns formula and id
+    def sample_ltl_goal(self):
+        return self.sampler.sample(), self.sampler.get_current_id()
