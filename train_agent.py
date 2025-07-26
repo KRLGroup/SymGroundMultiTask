3a0471b7cffde02728948adc416184c015ea3e3f
Matteo Pannacci <matteo.pannacci@gmail.com>
2025-07-26 20:30:07 +0200

    added frames of grounder pretrain to the count of total frames

--- train_agent.py
+++ train_agent.py
@@ -236,6 +236,7 @@ def train_agent(args: Args, device: str = None):
     elif use_grounder and args.use_pretrained_grounder:
         grounder_status = utils.get_status(pretrained_grounder_dir, device)
         sym_grounder.load_state_dict(grounder_status["grounder_state"])
+        status["num_frames"] += grounder_status["num_frames"]
         txt_logger.info("-) Loading grounder from pretrain.")
 
     sym_grounder.to(device) if sym_grounder is not None else None
